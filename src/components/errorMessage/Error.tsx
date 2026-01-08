f20281cd65e98b4e0327b5ebbb5ec673628af436
Sashka <sashkatmshchk@gmail.com>
2026-01-08 13:25:36 +0200

    Add data-cy to ErrorMessage

--- src/components/errorMessage/Error.tsx
+++ src/components/errorMessage/Error.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import classNames from 'classnames';
 
 type Props = {
-  error: string | null;
+  error: string;
   hideError: () => void;
 };
 
 export const ErrorMessage: React.FC<Props> = ({ error, hideError }) => {
   return (
     <div
-      className={classNames('notification is-danger is-light has-text-weight-normal', {
-        hidden: !error,
-      })}
+      data-cy="ErrorNotification"
+      className={classNames(
+        'notification is-danger is-light has-text-weight-normal',
+        { hidden: !error },
+      )}
     >
-      <button type="button" className="delete" onClick={hideError} />
+      <button
+        data-cy="HideErrorButton"
+        type="button"
+        className="delete"
+        onClick={hideError}
+      />
       {error}
     </div>
   );
